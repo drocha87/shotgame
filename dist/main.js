8a2fc2c4c40e0c805fc139f43ebb0704c78f1ac6
Diego Rocha <dlmrocha87@gmail.com>
2025-09-24 18:40:37 -0300

    put a lot of code, still a mess!

--- dist/main.js
+++ dist/main.js
@@ -11,19 +11,29 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const shotSound = new Audio("lmg_fire01.flac");
-const gameState = {
-    ctx: ctx,
-    mousePosition: { x: 0, y: 0 },
-    discs: [],
-    paused: true,
-    bullets: 10,
-};
+const loseSound = new Audio("lose sound 2 - 2.flac");
 var ShotAccuracy;
 (function (ShotAccuracy) {
     ShotAccuracy[ShotAccuracy["Missed"] = 0] = "Missed";
     ShotAccuracy[ShotAccuracy["Touch"] = 1] = "Touch";
     ShotAccuracy[ShotAccuracy["Perfect"] = 2] = "Perfect";
 })(ShotAccuracy || (ShotAccuracy = {}));
+const gameState = {
+    ctx: ctx,
+    mousePosition: { x: 0, y: 0 },
+    discs: [],
+    paused: true,
+    gameOver: false,
+    availableShots: 10,
+    shots: {
+        missed: 0,
+        good: 0,
+        perfect: 0,
+    },
+    lastSpawedDisc: 0,
+    accuracy: 0,
+    texts: [],
+};
 function shot(a, b, tolerance = 1) {
     const dx = b.x - a.x;
     const dy = b.y - a.y;
@@ -37,9 +47,9 @@ function shot(a, b, tolerance = 1) {
     }
     if (distanceSquared >= radiusDiffSquared &&
         distanceSquared <= radiusSumSquared) {
-        return ShotAccuracy.Perfect;
+        return ShotAccuracy.Touch;
     }
-    return ShotAccuracy.Touch;
+    return ShotAccuracy.Perfect;
 }
 function resizeCanvas(ctx) {
     const aspect = 16 / 9;
@@ -72,25 +82,80 @@ function renderCursor(ctx) {
 }
 function renderDiscs(state, delta) {
     for (const disc of state.discs) {
-        renderCircle(state.ctx, { x: disc.x, y: disc.y }, disc.radius, "red");
-        disc.x += delta * disc.velocity;
-        if (disc.x > canvas.width) {
-            // @todo: you missed this one handle it properly
+        renderCircle(state.ctx, { x: disc.x, y: disc.y }, disc.radius, "#fe9a00");
+        disc.duration -= delta;
+        if (disc.duration < 0) {
+            gameState.availableShots -= 1;
             disc.remove = true;
+            gameState.texts.push({
+                text: {
+                    text: "You missed!",
+                    x: disc.x - 20,
+                    y: disc.y + 20,
+                    font: "14px sans-serif",
+                    color: "red",
+                },
+                duration: 0.5,
+                remove: false,
+            });
+            loseSound.currentTime = 0;
+            loseSound.play();
         }
     }
 }
+function renderText(ctx, text) {
+    ctx.fillStyle = text.color;
+    ctx.font = text.font;
+    ctx.fillText(text.text, text.x, text.y);
+}
 function renderStats(ctx) {
-    ctx.fillStyle = "black";
-    ctx.font = "18px sans-serif";
-    ctx.fillText(`Bullets: ${gameState.bullets}`, 20, 30);
+    renderText(ctx, {
+        text: `Available Shots: ${gameState.availableShots}`,
+        x: 20,
+        y: 30,
+        font: "18px sans-serif",
+        color: "black",
+    });
+    const { good, missed, perfect } = gameState.shots;
+    const accuracy = (good + perfect) / (missed + good + perfect);
+    renderText(ctx, {
+        text: `Total Shots: ${good + perfect + missed}`,
+        x: 20,
+        y: 60,
+        font: "18px sans-serif",
+        color: "black",
+    });
+    renderText(ctx, {
+        text: `Accuracy: ${Math.floor(accuracy * 100)}%`,
+        x: 20,
+        y: canvas.height - 20,
+        font: "18px sans-serif",
+        color: "black",
+    });
 }
 function render(ctx, delta) {
     ctx.fillStyle = "#fafafa";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     renderStats(ctx);
     renderDiscs(gameState, delta);
+    for (const text of gameState.texts) {
+        if (text.duration <= 0) {
+            text.remove = true;
+            continue;
+        }
+        renderText(ctx, text.text);
+        text.duration -= delta;
+    }
     renderCursor(ctx);
+}
+function renderGameOver(ctx) {
+    ctx.fillStyle = "#fafafa";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    const gameOverStr = `GAME OVER`;
+    ctx.fillStyle = "black";
+    ctx.font = "24px sans-serif";
+    const metrics = ctx.measureText(gameOverStr);
+    ctx.fillText(gameOverStr, (canvas.width - metrics.width) / 2, (canvas.height - 24) / 2);
 }
 // Call once and on resize
 window.addEventListener("resize", () => {
@@ -105,12 +170,13 @@ function handleMouseClick(_event) {
     let accuracy = ShotAccuracy.Missed;
     shotSound.currentTime = 0;
     shotSound.play();
+    const aim = {
+        x: gameState.mousePosition.x,
+        y: gameState.mousePosition.y,
+        radius: 2,
+    };
     for (const disc of gameState.discs) {
-        accuracy = shot({
-            x: gameState.mousePosition.x,
-            y: gameState.mousePosition.y,
-            radius: 2,
-        }, disc);
+        accuracy = shot(aim, disc);
         if (accuracy === ShotAccuracy.Touch || accuracy === ShotAccuracy.Perfect) {
             disc.remove = true;
             break;
@@ -119,30 +185,77 @@ function handleMouseClick(_event) {
     switch (accuracy) {
         case ShotAccuracy.Missed:
             {
-                gameState.bullets -= 1;
+                gameState.availableShots -= 1;
+                gameState.shots.missed += 1;
+                gameState.texts.push({
+                    text: {
+                        text: "You missed!",
+                        x: aim.x - 20,
+                        y: aim.y + 20,
+                        font: "14px sans-serif",
+                        color: "black",
+                    },
+                    duration: 0.5,
+                    remove: false,
+                });
             }
             break;
         case ShotAccuracy.Touch:
             {
+                gameState.shots.good += 1;
+                gameState.texts.push({
+                    text: {
+                        text: "Good shot!",
+                        x: aim.x - 20,
+                        y: aim.y + 20,
+                        font: "14px sans-serif",
+                        color: "black",
+                    },
+                    duration: 0.5,
+                    remove: false,
+                });
             }
             break;
         case ShotAccuracy.Perfect:
             {
-                gameState.bullets += 1;
+                gameState.shots.perfect += 1;
+                gameState.availableShots += 1;
+                gameState.texts.push({
+                    text: {
+                        text: "Perfect shot!",
+                        x: aim.x - 20,
+                        y: aim.y + 20,
+                        font: "14px sans-serif",
+                        color: "black",
+                    },
+                    duration: 0.5,
+                    remove: false,
+                });
             }
             break;
     }
 }
+function clamp(value, min, max) {
+    return Math.min(Math.max(value, min), max);
+}
+function spawDisc() {
+    const rand = Math.random();
+    const padding = 100;
+    const radius = clamp(20 * rand, 5, 20);
+    const x = clamp(canvas.width * rand, radius + padding, canvas.width - radius - padding);
+    const y = clamp(canvas.height * rand, radius + padding, canvas.height - radius - padding);
+    gameState.discs.push({
+        x,
+        y,
+        radius,
+        velocity: 200 * rand,
+        remove: false,
+        duration: 2,
+    });
+}
 function handleKeyDown(event) {
     if (event.code === "Space") {
-        const rand = Math.random();
-        gameState.discs.push({
-            x: 0,
-            y: canvas.height * rand,
-            radius: 10,
-            velocity: 200 * rand,
-            remove: false,
-        });
+        spawDisc();
     }
 }
 canvas.addEventListener("click", () => __awaiter(void 0, void 0, void 0, function* () {
@@ -170,8 +283,20 @@ let previousTimestamp = 0;
 function gameLoop(timestamp) {
     const delta = (timestamp - previousTimestamp) / 1000;
     previousTimestamp = timestamp;
+    if (gameState.gameOver) {
+        renderGameOver(ctx);
+        return;
+    }
     if (!gameState.paused) {
         gameState.discs = gameState.discs.filter((x) => !x.remove);
+        gameState.texts = gameState.texts.filter((x) => !x.remove);
+        if (gameState.discs.length < 3 && gameState.lastSpawedDisc > 1) {
+            spawDisc();
+            gameState.lastSpawedDisc = 0;
+        }
+        else {
+            gameState.lastSpawedDisc += delta;
+        }
         render(ctx, delta);
     }
     requestAnimationFrame(gameLoop);
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 
 const shotSound = new Audio("lmg_fire01.flac");
+const loseSound = new Audio("lose sound 2 - 2.flac");
 
 type Disc = {
   x: number;
@@ -9,17 +10,14 @@ type Disc = {
   radius: number;
   velocity: number;
   remove: boolean;
+  duration: number; // stay alive in seconds
 };
 
-const gameState = {
-  ctx: ctx!,
-  mousePosition: { x: 0, y: 0 },
-  discs: [] as Array<Disc>,
-  paused: true,
-  bullets: 10,
+type Aim = {
+  x: number;
+  y: number;
+  radius: number;
 };
-
-type GameState = typeof gameState;
 
 enum ShotAccuracy {
   Missed,
@@ -27,11 +25,34 @@ enum ShotAccuracy {
   Perfect,
 }
 
-function shot(
-  a: Pick<Disc, "x" | "y" | "radius">,
-  b: Disc,
-  tolerance: number = 1
-): ShotAccuracy {
+type TText = {
+  text: string;
+  x: number;
+  y: number;
+  font: string;
+  color: string;
+};
+
+const gameState = {
+  ctx: ctx!,
+  mousePosition: { x: 0, y: 0 },
+  discs: [] as Array<Disc>,
+  paused: true,
+  gameOver: false,
+  availableShots: 10,
+  shots: {
+    missed: 0,
+    good: 0,
+    perfect: 0,
+  },
+  lastSpawedDisc: 0,
+  accuracy: 0,
+  texts: [] as Array<{ text: TText; duration: number; remove: boolean }>,
+};
+
+type GameState = typeof gameState;
+
+function shot(a: Aim, b: Disc, tolerance: number = 1): ShotAccuracy {
   const dx = b.x - a.x;
   const dy = b.y - a.y;
   const distanceSquared = dx * dx + dy * dy;
@@ -48,10 +69,10 @@ function shot(
     distanceSquared >= radiusDiffSquared &&
     distanceSquared <= radiusSumSquared
   ) {
-    return ShotAccuracy.Perfect;
+    return ShotAccuracy.Touch;
   }
 
-  return ShotAccuracy.Touch;
+  return ShotAccuracy.Perfect;
 }
 
 function resizeCanvas(ctx: CanvasRenderingContext2D) {
@@ -97,19 +118,62 @@ function renderCursor(ctx: CanvasRenderingContext2D) {
 
 function renderDiscs(state: GameState, delta: number) {
   for (const disc of state.discs) {
-    renderCircle(state.ctx, { x: disc.x, y: disc.y }, disc.radius, "red");
-    disc.x += delta * disc.velocity;
-    if (disc.x > canvas.width) {
-      // @todo: you missed this one handle it properly
+    renderCircle(state.ctx, { x: disc.x, y: disc.y }, disc.radius, "#fe9a00");
+    disc.duration -= delta;
+    if (disc.duration < 0) {
+      gameState.availableShots -= 1;
       disc.remove = true;
+
+      gameState.texts.push({
+        text: {
+          text: "You missed!",
+          x: disc.x - 20,
+          y: disc.y + 20,
+          font: "14px sans-serif",
+          color: "red",
+        },
+        duration: 0.5,
+        remove: false,
+      });
+      loseSound.currentTime = 0;
+      loseSound.play();
     }
   }
 }
 
+function renderText(ctx: CanvasRenderingContext2D, text: TText) {
+  ctx.fillStyle = text.color;
+  ctx.font = text.font;
+  ctx.fillText(text.text, text.x, text.y);
+}
+
 function renderStats(ctx: CanvasRenderingContext2D) {
-  ctx.fillStyle = "black";
-  ctx.font = "18px sans-serif";
-  ctx.fillText(`Bullets: ${gameState.bullets}`, 20, 30);
+  renderText(ctx, {
+    text: `Available Shots: ${gameState.availableShots}`,
+    x: 20,
+    y: 30,
+    font: "18px sans-serif",
+    color: "black",
+  });
+
+  const { good, missed, perfect } = gameState.shots;
+  const accuracy = (good + perfect) / (missed + good + perfect);
+
+  renderText(ctx, {
+    text: `Total Shots: ${good + perfect + missed}`,
+    x: 20,
+    y: 60,
+    font: "18px sans-serif",
+    color: "black",
+  });
+
+  renderText(ctx, {
+    text: `Accuracy: ${Math.floor(accuracy * 100)}%`,
+    x: 20,
+    y: canvas.height - 20,
+    font: "18px sans-serif",
+    color: "black",
+  });
 }
 
 function render(ctx: CanvasRenderingContext2D, delta: number) {
@@ -118,7 +182,35 @@ function render(ctx: CanvasRenderingContext2D, delta: number) {
 
   renderStats(ctx);
   renderDiscs(gameState, delta);
+
+  for (const text of gameState.texts) {
+    if (text.duration <= 0) {
+      text.remove = true;
+      continue;
+    }
+
+    renderText(ctx, text.text);
+    text.duration -= delta;
+  }
+
   renderCursor(ctx);
+}
+
+function renderGameOver(ctx: CanvasRenderingContext2D) {
+  ctx.fillStyle = "#fafafa";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const gameOverStr = `GAME OVER`;
+
+  ctx.fillStyle = "black";
+  ctx.font = "24px sans-serif";
+  const metrics = ctx.measureText(gameOverStr);
+
+  ctx.fillText(
+    gameOverStr,
+    (canvas.width - metrics.width) / 2,
+    (canvas.height - 24) / 2
+  );
 }
 
 // Call once and on resize
@@ -139,16 +231,14 @@ function handleMouseClick(_event: MouseEvent) {
   shotSound.currentTime = 0;
   shotSound.play();
 
-  for (const disc of gameState.discs) {
-    accuracy = shot(
-      {
-        x: gameState.mousePosition.x,
-        y: gameState.mousePosition.y,
-        radius: 2,
-      },
-      disc
-    );
+  const aim = {
+    x: gameState.mousePosition.x,
+    y: gameState.mousePosition.y,
+    radius: 2,
+  };
 
+  for (const disc of gameState.discs) {
+    accuracy = shot(aim, disc);
     if (accuracy === ShotAccuracy.Touch || accuracy === ShotAccuracy.Perfect) {
       disc.remove = true;
       break;
@@ -158,33 +248,94 @@ function handleMouseClick(_event: MouseEvent) {
   switch (accuracy) {
     case ShotAccuracy.Missed:
       {
-        gameState.bullets -= 1;
+        gameState.availableShots -= 1;
+        gameState.shots.missed += 1;
+
+        gameState.texts.push({
+          text: {
+            text: "You missed!",
+            x: aim.x - 20,
+            y: aim.y + 20,
+            font: "14px sans-serif",
+            color: "black",
+          },
+          duration: 0.5,
+          remove: false,
+        });
       }
       break;
 
     case ShotAccuracy.Touch:
       {
+        gameState.shots.good += 1;
+        gameState.texts.push({
+          text: {
+            text: "Good shot!",
+            x: aim.x - 20,
+            y: aim.y + 20,
+            font: "14px sans-serif",
+            color: "black",
+          },
+          duration: 0.5,
+          remove: false,
+        });
       }
       break;
 
     case ShotAccuracy.Perfect:
       {
-        gameState.bullets += 1;
+        gameState.shots.perfect += 1;
+        gameState.availableShots += 1;
+
+        gameState.texts.push({
+          text: {
+            text: "Perfect shot!",
+            x: aim.x - 20,
+            y: aim.y + 20,
+            font: "14px sans-serif",
+            color: "black",
+          },
+          duration: 0.5,
+          remove: false,
+        });
       }
       break;
   }
 }
 
+function clamp(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max);
+}
+
+function spawDisc() {
+  const rand = Math.random();
+
+  const padding = 100;
+  const radius = clamp(20 * rand, 5, 20);
+  const x = clamp(
+    canvas.width * rand,
+    radius + padding,
+    canvas.width - radius - padding
+  );
+  const y = clamp(
+    canvas.height * rand,
+    radius + padding,
+    canvas.height - radius - padding
+  );
+
+  gameState.discs.push({
+    x,
+    y,
+    radius,
+    velocity: 200 * rand,
+    remove: false,
+    duration: 2,
+  });
+}
+
 function handleKeyDown(event: KeyboardEvent) {
   if (event.code === "Space") {
-    const rand = Math.random();
-    gameState.discs.push({
-      x: 0,
-      y: canvas.height * rand,
-      radius: 10,
-      velocity: 200 * rand,
-      remove: false,
-    });
+    spawDisc();
   }
 }
 
@@ -218,8 +369,21 @@ function gameLoop(timestamp: DOMHighResTimeStamp) {
   const delta = (timestamp - previousTimestamp) / 1000;
   previousTimestamp = timestamp;
 
+  if (gameState.gameOver) {
+    renderGameOver(ctx!);
+    return;
+  }
+
   if (!gameState.paused) {
     gameState.discs = gameState.discs.filter((x) => !x.remove);
+    gameState.texts = gameState.texts.filter((x) => !x.remove);
+
+    if (gameState.discs.length < 3 && gameState.lastSpawedDisc > 1) {
+      spawDisc();
+      gameState.lastSpawedDisc = 0;
+    } else {
+      gameState.lastSpawedDisc += delta;
+    }
 
     render(ctx!, delta);
   }
